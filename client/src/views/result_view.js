79d2d6248d9cd6b62a6b719c306f37d422e51060
David A Pears <davidapears@gmail.com>
2018-08-18 16:37:18 +0100

    begin rendering info

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -15,9 +15,52 @@ ResultView.prototype.bindEvents = function () {
     console.log(evt.detail);
     this.container.innerHTML = "";
     const weatherReport = document.createElement("H3");
-    const date = timeConverter(evt.detail.currently.time);
-    weatherReport.textContent = `Typically on ${date}, the weather at this location is ${evt.detail.currently.summary}`;
+
+    const date = timeConverter(evt.detail.daily.data[0].time);
+
+    const icon = evt.detail.daily.data[0].icon;
+    weatherReport.textContent = `Typically on ${date}, the weather at this location is ${evt.detail.daily.data[0].summary}`;
     this.container.appendChild(weatherReport);
+    const weatherIcon = document.createElement('img');
+    weatherIcon.src = `images/weather_icons/${icon}.png`;
+    this.container.appendChild(weatherIcon);
+    // temperature for the afternoon
+    //const temperature = evt.detail.hourly.data[14]
+
+    const temp = farenToCelsius(evt.detail.hourly.data[14].temperature);
+
+    const temperature = document.createElement('p');
+    temperature.classList.add("temperature_day")
+    temperature.textContent = `${temp}C`
+    this.container.appendChild(temperature);
+
+    const rainChance = Math.round(evt.detail.daily.data[0].precipProbability*100);
+
+
+    const rainLogo = document.createElement('img');
+    rainLogo.src = 'images/weather_icons/rain_chance.png';
+    this.container.appendChild(rainLogo);
+    const rain = document.createElement("p");
+    rain.textContent = `${rainChance}%`
+    this.container.appendChild(rain);
+    const sunsetLogo = document.createElement('img');
+    sunsetLogo.src = 'images/weather_icons/sunset.png';
+    this.container.appendChild(sunsetLogo);
+
+
+    const sunsetTime = evt.detail.daily.data[0].sunsetTime;
+    const betterSunsetTime = timeConverterToHours(sunsetTime);
+    console.log(betterSunsetTime);
+    const actualSunsetTime = document.createElement('p');
+    actualSunsetTime.textContent = `Sunset at ${betterSunsetTime}`;
+    this.container.appendChild(actualSunsetTime)
+
+
+
+
+
+
+
   })
 
 
@@ -26,6 +69,8 @@ ResultView.prototype.bindEvents = function () {
   var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   var month = months[a.getMonth()];
   var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
   var time = date + ' ' + month  ;
   return time;
 }
@@ -33,6 +78,22 @@ console.log(timeConverter(0));
 
 
 
+
+function farenToCelsius(faren){
+  const celsius = Math.round(((faren - 32)/1.8));
+  return celsius;
+}
+
+
+
+function timeConverterToHours(UNIX_timestamp){
+var a = new Date(UNIX_timestamp * 1000);
+
+var hour = a.getHours()-12;
+var min = a.getMinutes();
+var time = hour + ' ' + min + 'pm'  ;
+return time;
+}
 
 
 
