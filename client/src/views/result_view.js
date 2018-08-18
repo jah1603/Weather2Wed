605b612f08bcb98ea6e56bce764ed275fc41e59b
David A Pears <davidapears@gmail.com>
2018-08-18 14:36:13 +0100

    Response sentence on results view working

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -6,19 +6,46 @@ const ResultView = function (container) {
   this.darkSky = new DarkSky();
 }
 
-
-
 ResultView.prototype.bindEvents = function () {
+
+
+
   PubSub.subscribe('DarkSky:weather-ready', (evt)=>{
 
     console.log(evt.detail);
     this.container.innerHTML = "";
     const weatherReport = document.createElement("H3");
-    weatherReport.textContent = evt.detail.currently.summary;
-
+    const date = timeConverter(evt.detail.currently.time);
+    weatherReport.textContent = `Typically on ${date}, the weather at this location is ${evt.detail.currently.summary}`;
     this.container.appendChild(weatherReport);
   })
 
+
+  function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var time = date + ' ' + month  ;
+  return time;
+}
+console.log(timeConverter(0));
+
+
+
+
+
+
+
 };
+
+
+
+
+
+
+
+
+
 
 module.exports = ResultView;
