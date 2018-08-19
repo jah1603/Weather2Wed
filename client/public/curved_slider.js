818e55c578902b7f971feb3cee39218b6483a517
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-19 22:33:59 +0100

    annotations in curved_slider.js and tidied up some of the code

--- client/public/curved_slider.js
+++ client/public/curved_slider.js
@@ -3,6 +3,8 @@ const curvedSlider = function(){
   this.data = null;
 }
 
+
+// Defines the parameters of the quadratic curve along which the selector ring will travel
 const curve = {
   x: 0,
   y: 50,
@@ -11,12 +13,12 @@ const curve = {
   endx: 450,
   endy: 50
 }
-let percentageAlongCurve = 0.45
+let percentageAlongCurve = 0.45 // point on curve where the selector ring sits initially
 
-let curveElement = document.getElementById('curve')
-let thumbElement = document.getElementById('thumb')
+let curveElement = document.querySelector('#curve')
+let ringElement = document.querySelector('#ring')
 
-// returns the X and Y coordinates at the stated percentage along curve
+// Returns the X and Y coordinates at the stated percentage along curve
 const getBezierQuadraticEquationXYCoordsAtPercent = (curve, percentageAlongCurve) => {
   let x = Math.pow(1 - percentageAlongCurve, 2) * curve.x + 2 * (1 - percentageAlongCurve) * percentageAlongCurve
     * curve.cpx + Math.pow(percentageAlongCurve, 2) * curve.endx
@@ -26,18 +28,20 @@ const getBezierQuadraticEquationXYCoordsAtPercent = (curve, percentageAlongCurve
   return { x, y }
 }
 
-const drawCurve = () => {
-  //sets attributes of SVG path element, where M means 'moveto' and Q denotes a quadratic Bézier equation
+const plotQuadraticCurve = () => {
+  //Sets attributes of SVG path element, where M means 'moveto' and Q denotes a quadratic Bézier equation
   curveElement.setAttribute(
     'd',
     `M${curve.x},${curve.y} Q${curve.cpx},${curve.cpy} ${curve.endx},${curve.endy}`
   )
 }
 
-
-const drawThumbUponInput = percentageAlongCurve => {
+// Automatically moves the selector ring in response to user input
+const updateSelectorRingPositionUponInput = percentageAlongCurve => {
 
 	let position = getBezierQuadraticEquationXYCoordsAtPercent(curve, percentageAlongCurve)
+
+// Mapping the value of the range input to the dates in one year based on the Julian calendar, using UNIX time
 
   const seconds = 1514818906 + percentageAlongCurve * 31536000;
   const dateToDisplay = new Date(seconds * 1000);
@@ -49,53 +53,43 @@ const drawThumbUponInput = percentageAlongCurve => {
 
    const formattedDate = date + ' ' + month;
 
-  document.getElementById('value').textContent = formattedDate;
+  document.querySelector('#value').textContent = formattedDate;
 
-  document.getElementById('value').style.webkitAnimationPlayState = "paused";
-  document.getElementById('value').style.animationPlayState = "paused";
+  document.querySelector('#value').style.webkitAnimationPlayState = "paused";
+  document.querySelector('#value').style.animationPlayState = "paused";
 
-  thumbElement.setAttribute('cx', position.x)
-  thumbElement.setAttribute('cy', position.y)
-  console.log(thumbElement);
+  ringElement.setAttribute('cx', position.x)
+  ringElement.setAttribute('cy', position.y)
+  console.log(ringElement);
 }
 
 
+// Defines the default (i.e. starting) position of the selector ring and displays an explanatory prompt to the user telling them how to set the date
+const initialSelectorRingPosition = percentageAlongCurve => {
 
-const drawThumb = percentageAlongCurve => {
+	const position = getBezierQuadraticEquationXYCoordsAtPercent(curve, percentageAlongCurve)
 
-	let position = getBezierQuadraticEquationXYCoordsAtPercent(curve, percentageAlongCurve)
+  document.querySelector('#value').textContent = "Slide ring to set a date";
 
-  const seconds = 1514818906 + percentageAlongCurve * 31536000;
-  const dateToDisplay = new Date(seconds * 1000);
-   var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
-   var month = months[dateToDisplay.getMonth()];
-   var date = dateToDisplay.getDate();
-
-   console.log(dateToDisplay);
-
-   const formattedDate = date + ' ' + month;
-
-  document.getElementById('value').textContent = "Slide ring to set a date";
-
-  thumbElement.setAttribute('cx', position.x)
-  thumbElement.setAttribute('cy', position.y)
-  console.log(thumbElement);
+  ringElement.setAttribute('cx', position.x) //Sets selector ring X position
+  ringElement.setAttribute('cy', position.y) //Sets selector ring Y position
+  console.log(ringElement);
 }
 
 const moveThumb = e => {
   console.log(e.target.value)
   percentageAlongCurve = e.target.value / 31536000
-  drawThumbUponInput(percentageAlongCurve)
+  updateSelectorRingPositionUponInput(percentageAlongCurve)
 }
 
 // event on the range input
-let rangeElement = document.getElementById('range')
+const rangeElement = document.querySelector('#range')
 rangeElement.value = percentageAlongCurve * 100
 rangeElement.addEventListener('input', moveThumb)
 
 
-// init
-drawCurve()
-drawThumb(percentageAlongCurve)
+// Initializes the curve and selector ring, the latter being updated constantly in response to user input
+plotQuadraticCurve()
+initialSelectorRingPosition(percentageAlongCurve)
 
 })
