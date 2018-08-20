4bdce5adb9f6f618e4432f39510e7c3e568dfc41
Stephen Rooney <s.f.rooney@gmail.com>
2018-08-20 09:50:33 +0100

    about to merge

--- client/src/views/more_information_view.js
+++ client/src/views/more_information_view.js
@@ -1,11 +1,11 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const MoreInformation = function (container, weatherData) {
+const MoreInformation = function(container, weatherData) {
   this.container = container;
   this.weatherData = weatherData;
 };
 
-MoreInformation.prototype.render = function () {
+MoreInformation.prototype.render = function() {
 
   const moreInfoContainer = document.createElement('div');
   moreInfoContainer.classList.add('more_information');
@@ -18,50 +18,50 @@ MoreInformation.prototype.render = function () {
   sunriseTime.textContent = `Sunrise at ${this.weatherData.daily.data[0].sunriseTime}`;
   moreInfoContainer.appendChild(sunriseTime);
 
-const moonPhaseLogo = document.createElement('img');
-moonPhaseLogo.src = 'images/weather_icons/moon.png';
-moreInfoContainer.appendChild(moonPhaseLogo);
-const moonPhase = document.createElement('p');
-moonPhase.textContent = `the moon phase is ${this.weatherData.daily.data[0].moonPhase}`;
-moreInfoContainer.appendChild(moonPhase);
+  const moonPhaseLogo = document.createElement('img');
+  moonPhaseLogo.src = 'images/weather_icons/moon.png';
+  moreInfoContainer.appendChild(moonPhaseLogo);
+  const moonPhase = document.createElement('p');
+  moonPhase.textContent = `the moon phase is ${this.weatherData.daily.data[0].moonPhase}`;
+  moreInfoContainer.appendChild(moonPhase);
 
 
-const tempHighAndLowLogo = document.createElement('img');
-tempHighAndLowLogo.src = 'images/weather_icons/temperature.png';
-moreInfoContainer.appendChild(tempHighAndLowLogo);
+  const tempHighAndLowLogo = document.createElement('img');
+  tempHighAndLowLogo.src = 'images/weather_icons/temperature.png';
+  moreInfoContainer.appendChild(tempHighAndLowLogo);
 
-const tempSummary = document.createElement('p');
-const highTime = this.weatherData.daily.data[0].temperatureHighTime;
-console.log(highTime);
-const lowTime = this.weatherData.daily.data[0].temperatureLowTime;
-const tempHigh = this.weatherData.daily.data[0].temperatureHigh;
-const tempLow = this.weatherData.daily.data[0].temperatureLow;
-tempSummary.textContent = `the day's lowest temperature is ${tempLow}C at ${lowTime} and the highest temperature will be ${tempHigh}C at ${highTime}`
-moreInfoContainer.appendChild(tempSummary);
-
-
+  const tempSummary = document.createElement('p');
+  const highTime = this.weatherData.daily.data[0].temperatureHighTime;
+  console.log(highTime);
+  const lowTime = this.weatherData.daily.data[0].temperatureLowTime;
+  const tempHigh = this.weatherData.daily.data[0].temperatureHigh;
+  const tempLow = this.weatherData.daily.data[0].temperatureLow;
+  tempSummary.textContent = `the day's lowest temperature is ${tempLow}C at ${lowTime} and the highest temperature will be ${tempHigh}C at ${highTime}`
+  moreInfoContainer.appendChild(tempSummary);
 
 
-const humidity = document.createElement('p');
-humidity.textContent = `humidity: ${this.weatherData.daily.data[0].humidity}`;
-moreInfoContainer.appendChild(humidity);
-
-const windLogo = document.createElement('img');
-windLogo.src = 'images/weather_icons/wind-speed.png'
-moreInfoContainer.appendChild(windLogo);
-const windSpeed = document.createElement('p');
-windSpeed.textContent = `Wind speed: ${this.weatherData.daily.data[0].windSpeed} mph`;
-moreInfoContainer.appendChild(windSpeed);
-
-const cloudLogo = document.createElement('img');
-cloudLogo.src = 'images/weather_icons/clouds.png'
-moreInfoContainer.appendChild(cloudLogo);
-const cloudCover = document.createElement('p');
-cloudCover.textContent = `cloud cover: ${this.weatherData.daily.data[0].cloudCover}%`;
-moreInfoContainer.appendChild(cloudCover);
 
 
-this.container.appendChild(moreInfoContainer)
+  const humidity = document.createElement('p');
+  humidity.textContent = `humidity: ${this.weatherData.daily.data[0].humidity}`;
+  moreInfoContainer.appendChild(humidity);
+
+  const windLogo = document.createElement('img');
+  windLogo.src = 'images/weather_icons/wind-speed.png'
+  moreInfoContainer.appendChild(windLogo);
+  const windSpeed = document.createElement('p');
+  windSpeed.textContent = `Wind speed: ${this.weatherData.daily.data[0].windSpeed} mph`;
+  moreInfoContainer.appendChild(windSpeed);
+
+  const cloudLogo = document.createElement('img');
+  cloudLogo.src = 'images/weather_icons/clouds.png'
+  moreInfoContainer.appendChild(cloudLogo);
+  const cloudCover = document.createElement('p');
+  cloudCover.textContent = `cloud cover: ${this.weatherData.daily.data[0].cloudCover}%`;
+  moreInfoContainer.appendChild(cloudCover);
+
+
+  this.container.appendChild(moreInfoContainer)
 
 
 
