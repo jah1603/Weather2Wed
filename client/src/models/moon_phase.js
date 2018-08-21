e8a4fa8c7f629cfb3cd0876b1bafe314ea2879aa
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-21 21:02:40 +0100

    initialised a loop to determine date for phase calculations

--- client/src/models/moon_phase.js
+++ client/src/models/moon_phase.js
@@ -6,9 +6,17 @@ moonPhaseMethods.prototype.calculateCoordinatesOfTheSun = function () {
 
 };
 
-moonPhaseMethods.prototype.convertDateEnteredByUserToNextInstanceOfThatDateInRealTime = function (date {
-  const secondsPerYear = 31536000;
-  const currentDateInUnix = Date.now().getTime()/1000;
+moonPhaseMethods.prototype.convertDateEnteredByUserToNextInstanceOfThatDateInRealTime = function (weddingDate) {
+
+  const secondsPerAstronomicalYear = 31557600;
+  const currentDateInUnix = Date.now();
+  const currentDateAtMidnight = currentDateInUnix.setHours(0, 0 , 0, 0).getTime()/1000;
+
+  // Initialize a series of if statements to ensure that the date used in the moon phase calculations is a future date
+
+  if (weddingDate < currentDateAtMidnight){
+    var dateForCalculations = currentDateAtMidnight + 31557600;
+  }
 };
 
 moonPhaseMethods.prototype.calculateMoonPhase = function () {
