48f49110c9fb65f9f10f47dc02826d7572dcfdf8
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-21 20:38:19 +0100

    created new moon phase methods model

---
+++ client/src/models/moon_phase.js
@@ -0,0 +1,16 @@
+const moonPhaseMethods = function() {
+};
+
+moonPhaseMethods.prototype.calculateCoordinatesOfTheSun = function () {
+
+};
+
+moonPhaseMethods.prototype.methodName = function () {
+
+};
+
+moonPhaseMethods.prototype.calculateVisiblePortionOfTheMoon = function () {
+
+};
+
+module.exports = moonPhaseMethods;
