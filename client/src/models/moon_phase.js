8718436cc2c8813f901b8bc75ae8ba7d59992001
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-21 20:53:22 +0100

    added const values

--- client/src/models/moon_phase.js
+++ client/src/models/moon_phase.js
@@ -7,7 +7,8 @@ moonPhaseMethods.prototype.calculateCoordinatesOfTheSun = function () {
 };
 
 moonPhaseMethods.prototype.convertDateEnteredByUserToNextInstanceOfThatDateInRealTime = function (date {
-  const currentDate
+  const secondsPerYear = 31536000;
+  const currentDateInUnix = Date.now().getTime()/1000;
 };
 
 moonPhaseMethods.prototype.calculateMoonPhase = function () {
