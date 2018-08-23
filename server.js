f6c7abee46d232be9bf2973996a42ecbc889fd1b
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-23 07:13:38 +0100

    heroku commit

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ var port = process.env.PORT || 8080;
 app.use(express.static('client/public'))
 app.use(express.static('client/src'))
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 app.get('/', function(req, res){
   res.sendFile('index.html');
 });
